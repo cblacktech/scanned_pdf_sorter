135e8f70085cf8c9a2690ced499ff01c57c72855
Caleb Black <cblacktech@gmail.com>
2020-07-21 14:26:37 -0700

    Comment clean-up

--- sorter_app_run.py
+++ sorter_app_run.py
@@ -4,6 +4,3 @@ from pdf_sorter_gui import SorterApp
 if __name__ == '__main__':
     app = SorterApp(tk.Tk())
     app.root.mainloop()
-
-    # crop_app = PdfCropSelector('pdf_sorter_app_tk/pdf_sorter_out/images')
-    # crop_app.activate()
