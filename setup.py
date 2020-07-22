f755ef988cfb160efca1198efe18a675bbe30b2a
Caleb Black <cblacktech@gmail.com>
2020-07-21 21:30:45 -0700

    Initial setup.py upload

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='scanned_pdf_sorter',
+    version='1.0.0.dev0',
+    packages=['scanned_pdf_sorter'],
+    url='https://gitlab.com/cblacktech/scanned_pdf_sorter',
+    license='',
+    author='Caleb Black',
+    author_email='cblacktech@gmail.com',
+    description='Extracts data from the pages of a pdf file and groups the pages by the data extracted',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    python_requires='~=3.0',
+    install_requires=[
+        'Pillow',
+        'pytesseract',
+        'pdf2image'
+    ],
+    zip_safe=False,
+)
