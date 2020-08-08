e8c1a6325e2cc35b0ef8092bc6d83cefbb296d33
Caleb Black <cblacktech@gmail.com>
2020-08-07 20:16:21 -0700

    Replacing README.md with README.rst, and refactoring README.rst

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 
 from os import path
 this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+with open(path.join(this_directory, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
     name='scanned_pdf_sorter',
-    version='1.0.0.dev0',
+    version='1.0.4',
     packages=['scanned_pdf_sorter'],
     url='https://gitlab.com/cblacktech/scanned_pdf_sorter',
     license='',
