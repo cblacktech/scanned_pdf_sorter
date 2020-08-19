15b74d8db4d58150b4b3ee4e9f3c63b42440edf4
Caleb Black <cblacktech@gmail.com>
2020-08-18 23:05:44 -0700

    Added SQL_SERVER section to the default config generator

--- scanned_pdf_sorter/pdf_image_config.py
+++ scanned_pdf_sorter/pdf_image_config.py
@@ -20,6 +20,16 @@ def default_config_create(filename='config.ini'):
         config.set('CROP_BOX', 'start_y', '0')
         config.set('CROP_BOX', 'end_x', '100')
         config.set('CROP_BOX', 'end_y', '100')
+        config.add_section('SQL_SERVER')
+        config.set('SQL_SERVER', 'driver', 'ODBC Driver 17 for SQL Server')
+        config.set('SQL_SERVER', 'server_ip', '172.17.0.2,1433')
+        config.set('SQL_SERVER', 'database', 'TestDB')
+        config.set('SQL_SERVER', 'table', 'Customers')
+        config.set('SQL_SERVER', 'id_column', 'id')
+        config.set('SQL_SERVER', 'email_column', 'email')
+        config.set('SQL_SERVER', 'sql_login', 'yes')
+        config.set('SQL_SERVER', 'username', 'SA')
+        config.set('SQL_SERVER', 'password', 'ToorPass39')
 
         with open(filename, 'w') as f:
             config.write(f)
