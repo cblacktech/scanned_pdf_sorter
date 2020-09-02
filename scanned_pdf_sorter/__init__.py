ab4baabdd26f996808cd5c75ca20f27b5c849598
Caleb Black <cblacktech@gmail.com>
2020-09-01 20:51:45 -0700

    Converted the single import statement to multiple statements in __init__.py

--- scanned_pdf_sorter/__init__.py
+++ scanned_pdf_sorter/__init__.py
@@ -1,6 +1,5 @@
-from scanned_pdf_sorter import (
-    pdf_image_config,
-    crop_box_selector,
-    pdf_image_viewer,
-    pdf_sorter_gui,
-)
+from . import crop_box_selector
+from . import mssql_query
+from . import pdf_image_config
+from . import pdf_image_viewer
+from . import pdf_sorter_gui
